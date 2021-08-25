945f5d0ab1bb8d1716b43fdc2190d42f3327168a
Keith Pine <keith.pine@gmail.com>
2021-08-25 09:32:18 -0700

    Improve options parsing
    Don't set any unspecified parameters.

--- options.js
+++ options.js
@@ -1,34 +1,42 @@
-// Setup keys based on the expected environment variables.
-// The server and driver don't allow blank or undefined keys,
-// so we need to pre-process the environment variables and
-// skip any undefined or blank ones. The server checks the key
-// lengths and converts to a Buffer for us.
-keys = {
-  S2_AccessControl: process.env.S2_ACCESS_CONTROL_KEY,
-  S2_Authenticated: process.env.S2_AUTHENTICATED_KEY,
-  S2_Unauthenticated: process.env.S2_UNAUTHENTICATED_KEY,
-  S0_Legacy: process.env.S0_LEGACY_KEY || process.env.NETWORK_KEY,
-};
-
-config = {
-  logConfig: {
-    filename: process.env.LOGFILENAME,
+function getLogConfig() {
+  config = {
     forceConsole: true,
-  },
+  };
 
+  if (process.env.LOGFILENAME) {
+    config.filename = process.env.LOGFILENAME;
+  }
+
+  return config;
+}
+
+// Obtain the security keys from the environment variables.
+// The server and driver don't allow blank or undefined keys,
+// so skip any unset/blank environment variables. The server checks
+// the key lengths and converts to a Buffer for us.
+function getSecurityKeys() {
+  const env_keys = {
+    S2_AccessControl: process.env.S2_ACCESS_CONTROL_KEY,
+    S2_Authenticated: process.env.S2_AUTHENTICATED_KEY,
+    S2_Unauthenticated: process.env.S2_UNAUTHENTICATED_KEY,
+    S0_Legacy: process.env.S0_LEGACY_KEY || process.env.NETWORK_KEY,
+  };
+
+  keys = {};
+  for (const [name, key] of Object.entries(env_keys)) {
+    if (key) {
+      keys[name] = key;
+    }
+  }
+
+  return keys;
+}
+
+module.exports = {
+  logConfig: getLogConfig(),
   storage: {
     cacheDir: "/cache",
     deviceConfigPriorityDir: "/cache/config",
   },
-
-  securityKeys: {},
+  securityKeys: getSecurityKeys(),
 };
-
-// Copy non-blank / undefined keys
-for (const [name, key] of Object.entries(keys)) {
-  if (key) {
-    config.securityKeys[name] = key;
-  }
-}
-
-module.exports = config;
