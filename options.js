340160d16da6052bc255602e90d79d0e0e948715
Keith Pine <keith.pine@gmail.com>
2021-08-24 23:12:23 -0700

    Properly handling unset security key environment variables

--- options.js
+++ options.js
@@ -1,4 +1,16 @@
-module.exports = {
+// Setup keys based on the expected environment variables.
+// The server and driver don't allow blank or undefined keys,
+// so we need to pre-process the environment variables and
+// skip any undefined or blank ones. The server checks the key
+// lengths and converts to a Buffer for us.
+keys = {
+  S2_AccessControl: process.env.S2_ACCESS_CONTROL_KEY,
+  S2_Authenticated: process.env.S2_AUTHENTICATED_KEY,
+  S2_Unauthenticated: process.env.S2_UNAUTHENTICATED_KEY,
+  S0_Legacy: process.env.S0_LEGACY_KEY || process.env.NETWORK_KEY,
+};
+
+config = {
   logConfig: {
     filename: process.env.LOGFILENAME,
     forceConsole: true,
@@ -9,10 +21,14 @@ module.exports = {
     deviceConfigPriorityDir: "/cache/config",
   },
 
-  securityKeys: {
-    S2_AccessControl: process.env.S2_ACCESS_CONTROL_KEY,
-    S2_Authenticated: process.env.S2_AUTHENTICATED_KEY,
-    S2_Unauthenticated: process.env.S2_UNAUTHENTICATED_KEY,
-    S0_Legacy: process.env.S0_LEGACY_KEY || process.env.NETWORK_KEY
-  },
+  securityKeys: {},
 };
+
+// Copy non-blank / undefined keys
+for (const [name, key] of Object.entries(keys)) {
+  if (key) {
+    config.securityKeys[name] = key;
+  }
+}
+
+module.exports = config;
