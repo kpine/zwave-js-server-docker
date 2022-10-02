55b1366b62137ada12952f5b226efc7e0785338c
Keith Pine <keith.pine@gmail.com>
2022-10-02 00:21:29 -0700

    More updates

--- scripts/zfw-update.sh
+++ scripts/zfw-update.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2312
 
-set -eu
+set -euo pipefail
 
 DEFAULT_DEV_PATH=/dev/ttyUSB0
+DEFAULT_FW_DIR=/tmp
 
-dev_path=${DEVICE_PATH:-${DEFAULT_DEV_PATH}}
-fw_dir=${FW_DIR:-}
+dev_path=${ZFWU_DEVICE_PATH:-${DEFAULT_DEV_PATH}}
+fw_dir=${ZFWU_FW_DIR:-${DEFAULT_FW_DIR}}
 
 function usage {
-  cat << EOF 2>&1
+  cat << EOF >&2
 USAGE:
     $(basename "$0") [-d DEVICE_PATH] [-f FW_DIR]
 
@@ -18,7 +20,7 @@ EOF
 }
 
 function help {
-  cat << EOF 2>&1
+  cat << EOF >&2
 $(basename "$0")
 Update Z-Wave controller firmware (700-series or later)
 
@@ -26,24 +28,32 @@ USAGE:
     $(basename "$0") [-d DEVICE-PATH] [-f FW-DIR]
 
 OPTIONS:
-   -d <device-path>     The Controller's serial device path. Also set with
-                        environment variable DEVICE_PATH.
+   -d <device-path>     The controller's serial device path. Also set with
+                        environment variable ZFWU_DEVICE_PATH.
                         [default: ${DEFAULT_DEV_PATH}]
    -f <fw-dir>          Optional default upload directory for minicom. Also set
-                        with environment variable FW_DIR. If not set the
-                        current directory will be used.
+                        with environment variable ZFWU_FW_DIR.
+                        [default: ${DEFAULT_FW_DIR}]
 EOF
   exit 1
 }
 
 function error {
-  echo "$@" >&2
+  for message; do
+    echo "${message}" >&2
+  done
+
   exit 1
 }
 
-if ! command -v minicom &> /dev/null; then
-  error "minicom was not found, please install it"
-fi
+function error_usage {
+  for message; do
+    echo "${message}" >&2
+  done
+  echo >&2
+
+  usage
+}
 
 while getopts :d:f:h arg; do
   case ${arg} in
@@ -58,34 +68,39 @@ while getopts :d:f:h arg; do
       fw_dir="${OPTARG}"
       ;;
     :)
-      printf "missing value for option -%s\n\n" "${OPTARG}"
-      usage
+      error_usage "missing value for option -${OPTARG}"
       ;;
     ?)
-      printf "invalid option: -%s\n\n" "${OPTARG}"
-      usage
+      error_usage "invalid option: -${OPTARG}"
       ;;
   esac
 done
 
-if [[ -z "${dev_path}" ]]; then
-  usage
+# If this is the HAOS SSH & Web Terminal add-on, install the software
+if [[ ${HOSTNAME} == "a0d7b954-ssh" ]] && command -v apk &> /dev/null; then
+  ! command -v minicom &>/dev/null && apk add minicom --no-cache --quiet --no-progress
+  ! command -v lsx &>/dev/null && apk add lrzsz --no-cache --quiet --no-progress --repository=https://dl-cdn.alpinelinux.org/alpine/edge/testing
 fi
 
-if [[ ! -c "${dev_path}" ]]; then
-  printf "Device path %s is not a valid serial device\n\n" "${dev_path}"
-  usage
-fi
+! command -v minicom &>/dev/null && error "minicom must be installed"
 
-if [[ ! -w "${dev_path}" || ! -r "${dev_path}" ]]; then
-    printf "No permission to read from or write to the serial device.\n"
-    exit 1
-fi
+# On Debian/Ubuntu, lrzsz xmodem is installed as `sx`.
+# On Alpine/MacOS, lrzsz xmodem is installed as `lsx`.
+# Find the correct one.
+SX_BIN=
+for x in sx lsx; do
+  ! sx_bin=$(command -v "${x}") && continue
+  ! grep -q 'sx.*lrzsz' <(${sx_bin} --version 2> /dev/null) && continue
+  SX_BIN="${sx_bin}"
+  break
+done
 
-if [[ -n "${fw_dir}" && ! -d "${fw_dir}" ]]; then
-  printf "Firmware directory %s is not valid\n\n" "${fw_dir}"
-  usage
-fi
+[[ ! -x ${SX_BIN} ]] && error "lrzsz must be installed"
+
+[[ ! -e "${dev_path}" ]] && error_usage "Device path ${dev_path} does not exist"
+[[ ! -c "${dev_path}" ]] && error_usage "Device path ${dev_path} is not a valid serial device"
+[[ ! -w "${dev_path}" || ! -r "${dev_path}" ]] && error_usage "No permission to read from or write to the serial device."
+[[ ! -d "${fw_dir}" ]] && error_usage "Firmware directory ${fw_dir} is not valid"
 
 cat << EOF > ~/.minirc.zwave
 # Machine-generated file - use "minicom -s" to change parameters.
@@ -97,18 +112,14 @@ pu pname6           YDNYN
 pu pname7           YUYNN
 pu pname8           NDYNN
 pu pname9           YUNYN
+pu pprog3           ${SX_BIN} -vv
+pu updir            ${fw_dir}
 pu baudrate         115200
 pu bits             8
 pu parity           N
 pu stopbits         1
 pu rtscts           No
 EOF
-
-if [[ -n "${fw_dir}" ]]; then
-  cat << EOF >> ~/.minirc.zwave
-pu updir            ${fw_dir}
-EOF
-fi
 
 zwave_runscript=$(cat <<EOF
 print Activating the controller's bootloader. It will appear in about 10 seconds...
